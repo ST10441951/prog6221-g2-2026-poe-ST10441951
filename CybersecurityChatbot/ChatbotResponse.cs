9b139e24948880533489ccd8dba7be5214a1609d
Joshua Marc Lourens <160500140+ST10441951@users.noreply.github.com>
2026-03-31 08:09:06 +0200

    ChatbotResponse class added
    Added ChatbotResponse class with automatic properties

---
+++ CybersecurityChatbot/ChatbotResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CybersecurityChatbot
+{
+    // This class holds the data for specific cybersecurity topics
+    public class ChatbotResponse
+    {
+        // Automatic Properties
+        public string Topic { get; set; }
+        public string Information { get; set; }
+
+        // Constructor to initialize the topic
+        public ChatbotResponse(string topic, string information)
+        {
+            this.Topic = topic;
+            this.Information = information;
+        }
+    }
+}
